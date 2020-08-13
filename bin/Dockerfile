6e7ab88e15a4888f7337ab4d56ab2fa5db666871
sm90 <marioal_2008@hotmail.com>
2020-08-13 08:33:30 -0400

    commit for deploy to heroku

---
+++ bin/Dockerfile
@@ -0,0 +1,3 @@
+FROM open-jdk:8-jdk-alpine 
+COPY target/spring-boot-data-jpa-*.jar /app.jar
+ENTRYPOINT java -jar /app.jar
---
+++ target/spring-boot-data-jpa-1.0/WEB-INF/classes/import.sql
@@ -0,0 +1,16 @@
+/* Populate tables */
+INSERT INTO users(name, lastname, email, created, modified, photo) VALUES ('Chuck', 'Norris', 'chucknorris@gmail.com', now(), now(), '');
+INSERT INTO users(name, lastname, email, created, modified, photo) VALUES ('Juanito', 'Perez', 'juanito.perez@gmail.com', now(), now(), '');
+
+INSERT INTO products(name, price, stock, created, photo) VALUES ('Audifonos HP', 18990, 45, now(), 'audifonos.png');
+INSERT INTO products(name, price, stock, created, photo) VALUES ('Mouse Gamer Redragon', 14990, 50, now(), 'mouse.jpg');
+INSERT INTO products(name, price, stock, created, photo) VALUES ('LG SmartTV 55', 199990, 20, now(), 'smartTV.jpg');
+INSERT INTO products(name, price, stock, created, photo) VALUES ('Mousepad Genius', 8990, 100, now(), 'mousepad.jpg');
+INSERT INTO products(name, price, stock, created, photo) VALUES ('Teclado Gamer Redragon', 21990, 100, now(), 'teclado.jpg');
+
+INSERT INTO bills(description, created, user_id) VALUES('GAMER Pack 2020', now(), 1);
+
+INSERT INTO bill_details(quantity, product_id, bill_id) VALUES (5, 1, 1);
+INSERT INTO bill_details(quantity, product_id, bill_id) VALUES (2, 2, 1);
+INSERT INTO bill_details(quantity, product_id, bill_id) VALUES (1, 4, 1);
+INSERT INTO bill_details(quantity, product_id, bill_id) VALUES (3, 5, 1);
